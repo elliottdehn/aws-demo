e516506274e492dae29d12affcef62d056419afc
elliottdehnbostel <dehnbostele@gmail.com>
2020-07-30 20:14:03 -0400

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_0NRuXSwCL', // e.g. us-east-2_0NRuXSwCL
+        userPoolClientId: '65vfihmb0rl3m1s4hnl23oc0ue',
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
